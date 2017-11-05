3c053ae947591a18e14ad2cc05f8fe9e229437a2
Arkadiusz Cacko <cackoarek@gmail.com>
2017-11-05 16:56:34 +0100

    MVC - generowanie widoku z modelu

--- src/main/java/pl/kodolamacz/mvc/controllers/UserController.java
+++ src/main/java/pl/kodolamacz/mvc/controllers/UserController.java
@@ -1,20 +1,26 @@
 package pl.kodolamacz.mvc.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kodolamacz.spring.dao.repository.UserDao;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by acacko on 05.11.17
  */
-
 @Controller
-public class HomeController {
+@RequestMapping("accounts")
+public class UserController {
+
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(name = "/", method = GET)
     public String get(Model model) {
-        return "home";
+        model.addAttribute("users", userDao.findAll());
+        return "accounts";
     }
 }
