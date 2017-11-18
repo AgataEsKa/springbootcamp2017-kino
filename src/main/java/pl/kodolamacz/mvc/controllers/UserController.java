6013bc6e6d8de3ad840460076797dc581cca0140
Arkadiusz Cacko <cackoarek@gmail.com>
2017-11-18 14:29:27 +0100

    Obsługa formularzy - metoda POST

--- src/main/java/pl/kodolamacz/mvc/controllers/UserController.java
+++ src/main/java/pl/kodolamacz/mvc/controllers/UserController.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kodolamacz.mvc.controllers.forms.AddUserForm;
+import pl.kodolamacz.spring.dao.model.User;
 import pl.kodolamacz.spring.dao.repository.UserDao;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static pl.kodolamacz.mvc.controllers.UserController.BASE_URL;
 
 /**
  * Created by acacko on 05.11.17
  */
 @Controller
-@RequestMapping("accounts")
+@RequestMapping(BASE_URL)
 public class UserController {
+
+  public static final String BASE_URL = "accounts";
 
     @Autowired
     private UserDao userDao;
@@ -21,6 +27,20 @@ public class UserController {
     @RequestMapping(method = GET)
     public String get(Model model) {
         model.addAttribute("users", userDao.findUsersFetchResevation());
-        return "accounts";
+        return BASE_URL;
     }
+
+
+    @RequestMapping(value = "/add", method = GET)
+    public String addNewUser(Model model) {
+      model.addAttribute("userForm", new AddUserForm());
+      return "addNewUser";
+    }
+
+  @RequestMapping(value = "/save", method = POST)
+  public String saveNewUser(AddUserForm userForm) {
+    userDao.save(new User(userForm.getEmail(), userForm.getPassword()));
+    return "redirect:/" + BASE_URL;
+  }
+
 }
---
+++ src/main/java/pl/kodolamacz/mvc/controllers/forms/AddUserForm.java
@@ -0,0 +1,27 @@
+package pl.kodolamacz.mvc.controllers.forms;
+
+/**
+ * Created by acacko on 18.11.17
+ */
+public class AddUserForm {
+
+  private String email;
+
+  private String password;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
