443f5b8da14c47e287b7bc2b41f17e13c22b2d8c
Arkadiusz Cacko <cackoarek@gmail.com>
2017-11-18 12:37:15 +0100

    Przekazywanie parametrów poprzez GET -> PathVariable

--- src/main/java/pl/kodolamacz/mvc/controllers/ReservationController.java
+++ src/main/java/pl/kodolamacz/mvc/controllers/ReservationController.java
@@ -3,6 +3,7 @@ package pl.kodolamacz.mvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +33,17 @@ public class ReservationController {
   public String getReservationsByUserId(Model model, @RequestParam("userId") Long userId) {
     // jeśli paramName oraz nazwa zmiennej się pokrywają.
     // Można pominąć deklarację nazwy zmiennej w @RequestParam
+    model.addAttribute("userReservations", reservationDao.findByUserId(userId));
+    model.addAttribute("user", userDao.findOne(userId));
+    return "reservations";
+  }
+
+  // przykład adresu: http://localhost:8080/kino/reservations/byUser2/2
+  @RequestMapping(value = "/byUser2/{userId}", method = RequestMethod.GET)
+  public String getReservationsByUserId2(Model model, @PathVariable("userId") Long userId) { // parametrów może być więcej
+    // jeśli paramName oraz nazwa zmiennej się pokrywają.
+    // Można pominąć deklarację nazwy zmiennej w @PathVariable
+    // wiecej parametrów np.: http://localhost:8080/kino/reservations/byUser2/2/Arek/1988
     model.addAttribute("userReservations", reservationDao.findByUserId(userId));
     model.addAttribute("user", userDao.findOne(userId));
     return "reservations";
