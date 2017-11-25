ece3856428d361d31023b8229d99a48d4b6e1d47
Arkadiusz Cacko <cackoarek@gmail.com>
2017-11-25 12:21:46 +0100

    @Spy - nieudany - do domu

---
+++ src/test/java/pl/kodolamacz/spring/CinemaServiceSpyTest.java
@@ -0,0 +1,31 @@
+package pl.kodolamacz.spring;
+
+/**
+ * Created by acacko on 25.11.17
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("classpath:application.xml")
+//public class CinemaServiceSpyTest {
+//
+//    @Spy
+//    private UserDaoImpl userDao;
+//
+//    @Before
+//    public void init() {
+//        MockitoAnnotations.initMocks(this);
+//    }
+//
+//
+//    @Test
+//    public void testWithMockito() throws UserNotFoundException {
+//        User user = new User();
+//        when(userDao.findByPaaasword("aa")).thenReturn(user);
+//
+//        assertThat(userDao.findByPaaasword("aa")).isEqualTo(user);
+//
+//
+//
+//
+//    }
+//
+//}
